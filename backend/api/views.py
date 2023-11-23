ee03545b115f8f237effa1d8bbf8ff170eca862f
your_github_username <jakub.galuszka@interia.pl>
2023-11-23 23:11:42 +0100

    serializers init

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from products.models import Product
+from products.serializers import ProductSerializer
 
 # Create your views here.
 
+
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hello response"})
+
+
+    """
+    DRF API View
+    """
+    instance = Product.objects.all().order_by("?").first()
+    data = {}
+    if instance:
+        data = ProductSerializer(instance).data
+
+    return Response(data)
 
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,5 +3,5 @@ import requests
 endpoint = "http://localhost:8000/api"
 #endpoint = "https://httpbin.com/anything"
 
-get_response = requests.get(endpoint)
-print(get_response.text)
+get_response = requests.get(endpoint, params={"abc": 123}, json={"message": "Hello"})
+print(get_response.json())
